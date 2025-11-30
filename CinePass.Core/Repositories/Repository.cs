45cf36f83a6d7b581fd5ca0042b92d70e8ee24e8
lowjichow <phucloidanghoconline@gmail.com>
2025-11-30 14:28:21 +0700

    update migration, program,add interface repo, add UnitOfWork

---
+++ CinePass.Core/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq.Expressions;
+using CinePass.Core.Configurations;
+using CinePass.Domain.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinePass.Core.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public Repository(AppDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public virtual async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<T> AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        return entity;
+    }
+
+    public virtual async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await Task.CompletedTask;
+    }
+
+    public virtual async Task DeleteAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+
+    public virtual async Task<bool> ExistsAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+        return entity != null;
+    }
+}
---
+++ CinePass.Domain/IRepository/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿namespace CinePass.Domain.IRepository;
+
+public interface IUnitOfWork : IDisposable
+{
+    IMovieRepository Movies { get; }
+    IShowtimeRepository Showtimes { get; }
+    IBookingRepository Bookings { get; }
+    IBookingDetailRepository BookingDetails { get; }
+    ISeatRepository Seats { get; }
+    IScreenRepository Screens { get; }
+    ICinemaRepository Cinemas { get; }
+    IPaymentRepository Payments { get; }
+    // IUserRepository Users { get; }
+    
+    Task<int> SaveChangesAsync();
+    Task BeginTransactionAsync();
+    Task CommitTransactionAsync();
+    Task RollbackTransactionAsync();
+}
